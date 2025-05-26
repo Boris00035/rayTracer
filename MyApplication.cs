cac654e3575c175d394419b1d5d3aa9e6f61a63c
Boris <borisvanboxtel@protonmail.com>
2025-05-26 11:07:00 +0200

    sync

--- MyApplication.cs
+++ MyApplication.cs
@@ -11,6 +11,8 @@
 // waarom eindigt mijn plane altijd in het midden van het scherm?
 // waarom is de rand van de bollen zo pixelated maar van de specular spots niet?
 
+// parallel for ergens in gooien
+
 using Microsoft.VisualBasic;
 using OpenTK.Mathematics;
 using System.Diagnostics;
@@ -239,9 +241,12 @@ namespace Template
             float uDistance = Vector3.Dot(localVector, u);
             float vDistance = Vector3.Dot(localVector, v);
 
+            if (width != 0 && height != 0)
+            {
             // checken of snijpunt binnen vlak valt
             if (Math.Abs(uDistance) > width / 2f || Math.Abs(vDistance) > height / 2f)
                 return null; // Buiten het vlak
+            }
 
 
             Vector3 surfaceNormal = this.normal;
@@ -748,8 +753,8 @@ namespace Template
                     new Color3(1,1,0),
                     new Color3(1,1,1),
                     new Vector3(0, 1, 0),
-                    20,
-                    20,
+                    0,
+                    0,
                     false, [])
             ],
             [
